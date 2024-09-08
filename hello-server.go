13539b0fbbaf60551d86faa6bc4a1436d839639d
DenisImpacta <denis.guedes@aluno.faculdadeimpacta.com.br>
2024-09-08 19:45:58 -0300

    Update hello-server.go

--- hello-server.go
+++ hello-server.go
@@ -4,26 +4,15 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    
-    // Adicionando verificação de erro na inicialização do servidor
-    err := http.ListenAndServe(":13000", nil)
-    if err != nil {
-        log.Fatalf("Erro ao iniciar o servidor: %v", err)
-    }
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":13000", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    // Adicionando uma verificação simples para um path vazio
-    if r.URL.Path == "/" {
-        fmt.Fprintf(w, "Hello, World!")
-    } else {
-        fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-    }
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
